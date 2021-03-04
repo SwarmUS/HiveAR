8e9da82f7f86a35c82c950176489d05992b0127c
Thierry Constantin <thierry.constantin@usherbrooke.ca>
2021-03-04 12:48:13 -0500

    Detect images

--- HiveAR/app/src/main/java/com/swarmus/hivear/fragments/ARViewFragment.java
+++ HiveAR/app/src/main/java/com/swarmus/hivear/fragments/ARViewFragment.java
@@ -1,19 +1,99 @@
 package com.swarmus.hivear.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.ux.ArFragment;
 import com.swarmus.hivear.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashMap;
+
 public class ARViewFragment extends Fragment {
+
+    ArFragment arFragment;
+    HashMap<String, AugmentedImage.TrackingMethod> trackableInfos;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.ar_view_fragment, container, false);
+
+        trackableInfos = new HashMap<>();
+
+        arFragment = (ArFragment)getChildFragmentManager().findFragmentById(R.id.ux_fragment);
+        if (arFragment!=null) {
+            Session session = arFragment.getArSceneView().getSession();
+            if (session == null) {
+                try {
+                    session = new Session(requireContext());
+                } catch (UnavailableArcoreNotInstalledException e) {
+                    e.printStackTrace();
+                } catch (UnavailableApkTooOldException e) {
+                    e.printStackTrace();
+                } catch (UnavailableSdkTooOldException e) {
+                    e.printStackTrace();
+                } catch (UnavailableDeviceNotCompatibleException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                InputStream inputStream = requireContext().getAssets().open(getString(R.string.tags_db));
+                Config config = new Config(session);
+                config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+                config.setAugmentedImageDatabase(AugmentedImageDatabase.deserialize(session, inputStream));
+                config.setFocusMode(Config.FocusMode.AUTO);
+                config.setPlaneFindingMode(Config.PlaneFindingMode.HORIZONTAL);
+                session.configure(config);
+                arFragment.getArSceneView().setupSession(session);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+                updateFrame(frameTime);
+            });
+        }
+
         return view;
+    }
+
+    // More details here: https://medium.com/free-code-camp/how-to-build-an-augmented-images-application-with-arcore-93e417b8579d
+    private void updateFrame(FrameTime frameTime) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+        // Set trackable
+        Collection<AugmentedImage> augmentedImages = frame.getUpdatedTrackables(AugmentedImage.class);
+        for (AugmentedImage augmentedImage : augmentedImages) {
+            AugmentedImage.TrackingMethod currentMethod = augmentedImage.getTrackingMethod();
+            if (!trackableInfos.containsKey(augmentedImage.getName())) {
+                trackableInfos.put(augmentedImage.getName(), augmentedImage.getTrackingMethod());
+            }
+            if (currentMethod != trackableInfos.get(augmentedImage.getName())) {
+                trackableInfos.replace(augmentedImage.getName(), currentMethod);
+                String msg = augmentedImage.getName() + " : " +currentMethod.toString();
+                // Show to user if currently tracking
+                if (currentMethod.equals(AugmentedImage.TrackingMethod.FULL_TRACKING)) {
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_LONG).show();
+                }
+                Log.e("ARCore", msg);
+            }
+        }
     }
 }
