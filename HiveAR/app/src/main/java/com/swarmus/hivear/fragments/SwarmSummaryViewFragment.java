e29fac1a93cc58adebbdff2216264d2fda17f8c6
Thierry Constantin <thierry.constantin@usherbrooke.ca>
2021-03-28 14:43:11 -0400

    remvoed unused buzz command

--- HiveAR/app/src/main/java/com/swarmus/hivear/fragments/SwarmSummaryViewFragment.java
+++ HiveAR/app/src/main/java/com/swarmus/hivear/fragments/SwarmSummaryViewFragment.java
@@ -9,11 +9,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.swarmus.hivear.R;
-import com.swarmus.hivear.activities.MainActivity;
 import com.swarmus.hivear.adapters.ViewRobotListAdapter;
-import com.swarmus.hivear.commands.UpdateSwarmContentCommand;
 import com.swarmus.hivear.models.RobotListViewModel;
 
 public class SwarmSummaryViewFragment extends Fragment {
@@ -21,11 +18,6 @@ public class SwarmSummaryViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.swarm_summary_view_fragment, container, false);
-
-        FloatingActionButton updateSwarm = view.findViewById(R.id.updateSwarm);
-
-        UpdateSwarmContentCommand updateSwarmContentCommand = new UpdateSwarmContentCommand();
-        updateSwarm.setOnClickListener(v -> {((MainActivity)requireActivity()).sendCommand(updateSwarmContentCommand);});
 
         updateRobotListView(view);
         RobotListViewModel robotListViewModel = new ViewModelProvider(requireActivity()).get(RobotListViewModel.class);
