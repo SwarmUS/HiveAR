7929a227ee161ea4e1e72a47a9be576f14ab8e25
Thierry Constantin <thierry.constantin@usherbrooke.ca>
2021-04-08 22:11:31 -0400

    Color in text

--- HiveAR/app/src/main/java/com/swarmus/hivear/fragments/ConnectionViewFragment.java
+++ HiveAR/app/src/main/java/com/swarmus/hivear/fragments/ConnectionViewFragment.java
@@ -6,13 +6,11 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -49,10 +47,10 @@ public class ConnectionViewFragment extends Fragment {
         TextView dataReceived = view.findViewById(R.id.dataReceived);
         dataReceived.setMovementMethod(new ScrollingMovementMethod());
         ProtoMsgViewModel protoMsgViewModel = new ViewModelProvider(requireActivity()).get(ProtoMsgViewModel.class);
-        dataReceived.setText(protoMsgViewModel.getLastMsgs(MSG_LOGGING_LENGTH));
+        dataReceived.setText(protoMsgViewModel.getLastMsgsSpannable(MSG_LOGGING_LENGTH));
         protoMsgViewModel.getMsgQueue().observe(getViewLifecycleOwner(), s -> {
             int scrollY = dataReceived.getScrollY();
-            dataReceived.setText(protoMsgViewModel.getLastMsgs(MSG_LOGGING_LENGTH));
+            dataReceived.setText(protoMsgViewModel.getLastMsgsSpannable(MSG_LOGGING_LENGTH));
             scrollY = Math.max(scrollY, 0);
             final Layout layout = dataReceived.getLayout();
             if(layout != null){
--- HiveAR/app/src/main/java/com/swarmus/hivear/viewmodels/ProtoMsgViewModel.java
+++ HiveAR/app/src/main/java/com/swarmus/hivear/viewmodels/ProtoMsgViewModel.java
@@ -1,5 +1,10 @@
 package com.swarmus.hivear.viewmodels;
 
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -15,6 +20,16 @@ public class ProtoMsgViewModel extends ViewModel {
     private final MutableLiveData<LinkedList<String>> msgQueue = new MutableLiveData<>(new LinkedList<>());
     private final int maxCapacity = 10; // We keep last 10 messages
     private final static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+    private static String requestPattern = "request";
+    private static String responsePattern = "response";
+    private static String greetingPattern = "greeting";
+    private static String nullPattern = "null";
+
+    private static int requestColor = Color.BLUE;
+    private static int responseColor = Color.GREEN;
+    private static int greetingColor = Color.YELLOW;
+    private static int invalidColor = Color.RED;
 
     public LiveData<LinkedList<String>> getMsgQueue() { return msgQueue; }
 
@@ -41,5 +56,45 @@ public class ProtoMsgViewModel extends ViewModel {
             logMsg += msgs.get(i) + "\n";
         }
         return logMsg;
+    }
+
+    public SpannableString getLastMsgsSpannable(int nbToRetrieve) {
+
+        String lastMsgs = getLastMsgs(nbToRetrieve);
+
+        SpannableString s = new SpannableString(lastMsgs);
+        String[] parts = lastMsgs.split("\n");
+        int lastResponse = 0;
+        int lastRequest = 0;
+        int lastGreeting = 0;
+        int lastNull = 0;
+        for(final String word : parts) {
+            if(word.contains(responsePattern)) {
+                s.setSpan(new ForegroundColorSpan(responseColor),
+                        lastMsgs.indexOf(responsePattern, lastResponse),
+                        lastMsgs.indexOf(responsePattern, lastResponse) + responsePattern.length(),
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                lastResponse = lastMsgs.indexOf(responsePattern, lastResponse)+1;
+            } else if(word.contains(requestPattern)) {
+                s.setSpan(new ForegroundColorSpan(requestColor),
+                        lastMsgs.indexOf(requestPattern, lastRequest),
+                        lastMsgs.indexOf(requestPattern, lastRequest) + requestPattern.length(),
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                lastRequest = lastMsgs.indexOf(requestPattern, lastRequest)+1;
+            } else if(word.contains(greetingPattern)) {
+                s.setSpan(new ForegroundColorSpan(greetingColor),
+                        lastMsgs.indexOf(greetingPattern, lastGreeting),
+                        lastMsgs.indexOf(greetingPattern, lastGreeting) + greetingPattern.length(),
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                lastGreeting = lastMsgs.indexOf(greetingPattern, lastGreeting)+1;
+            } else if(word.contains(nullPattern)) {
+                s.setSpan(new ForegroundColorSpan(invalidColor),
+                        lastMsgs.indexOf(nullPattern, lastNull),
+                        lastMsgs.indexOf(nullPattern, lastNull) + nullPattern.length(),
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                lastNull = lastMsgs.indexOf(nullPattern, lastNull)+1;
+            }
+        }
+        return s;
     }
 }
