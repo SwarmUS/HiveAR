b9c5509e8f3ae3fa8d0c8e4519bccc352c9d90be
Thierry Constantin <thierry.constantin@usherbrooke.ca>
2021-04-20 14:27:55 -0400

    Connection fragment better code

--- HiveAR/app/src/main/java/com/swarmus/hivear/fragments/ConnectionViewFragment.java
+++ HiveAR/app/src/main/java/com/swarmus/hivear/fragments/ConnectionViewFragment.java
@@ -25,6 +25,7 @@ import com.swarmus.hivear.R;
 import com.swarmus.hivear.activities.MainActivity;
 import com.swarmus.hivear.commands.MoveByCommand;
 import com.swarmus.hivear.models.CommunicationDevice;
+import com.swarmus.hivear.models.ProtoMsgStorer;
 import com.swarmus.hivear.models.SerialDevice;
 import com.swarmus.hivear.models.TCPDeviceServer;
 import com.swarmus.hivear.viewmodels.ProtoMsgViewModel;
@@ -41,7 +42,6 @@ public class ConnectionViewFragment extends Fragment {
     private ProtoMsgViewModel protoMsgViewModel;
     private TextView dataReceived;
 
-    private FragmentManager fragmentManager;
     private static final int MSG_LOGGING_LENGTH = 10;
     private boolean isInfoVisible = false;
 
@@ -49,71 +49,9 @@ public class ConnectionViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.connection_view_fragment, container, false);
 
-        fragmentManager = getChildFragmentManager();
-
         protoMsgViewModel = new ViewModelProvider(requireActivity()).get(ProtoMsgViewModel.class);
 
-        Spinner dropdown = view.findViewById(R.id.loggingFilter);
-        protoMsgViewModel.getProtoMsgStorerList().observe(getViewLifecycleOwner(), storers -> {
-            ArrayList<String> storerNames = new ArrayList<>();
-            storers.forEach(protoMsgStorer -> storerNames.add(protoMsgStorer.getUniqueName()));
-            ArrayAdapter<String> adapter =  new ArrayAdapter<>(requireActivity().getBaseContext(), android.R.layout.simple_spinner_dropdown_item, storerNames.toArray(new String[0]));
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            dropdown.setAdapter(adapter);
-            if (protoMsgViewModel.getCurrentProtoMsgStorer().getValue() != null) {
-                String selectedDevice = protoMsgViewModel.getCurrentProtoMsgStorer().getValue().getUniqueName();
-                if (selectedDevice != null) {
-                    dropdown.setSelection(adapter.getPosition(selectedDevice));
-                }
-            }
-        });
-        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View currentView, int i, long l) {
-                protoMsgViewModel.setCurrentProtoMsgStorer( protoMsgViewModel.getProtoMsgStorerList().getValue().get(i));
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {}
-        });
-
         dataReceived = view.findViewById(R.id.dataReceived);
-        dataReceived.setMovementMethod(new ScrollingMovementMethod());
-
-        dataReceived.setText(protoMsgViewModel.getLastMsgsSpannable(MSG_LOGGING_LENGTH));
-        // Align text correctly on change logging filter
-        protoMsgViewModel.getCurrentProtoMsgStorer().observe(getViewLifecycleOwner(), s -> { filterChanged(); });
-
-        // Init filter dropdown
-        filterChanged();
-
-        view.findViewById(R.id.hide_ui).setOnClickListener(v -> {
-            setInfoVisible(!isInfoVisible);
-        });
-
-        view.findViewById(R.id.clean_text).setOnClickListener(v -> {
-            protoMsgViewModel.clearLogs();
-            dataReceived.setText("");
-        });
-
-        view.findViewById(R.id.connectButton).setOnClickListener(v -> {
-            CommunicationDevice communicationDevice = ((MainActivity)getActivity()).getCurrentCommunicationDevice();
-            if (communicationDevice != null) {
-                communicationDevice.establishConnection();
-            }
-        });
-        view.findViewById(R.id.disconnectButton).setOnClickListener(v -> {
-            CommunicationDevice communicationDevice = ((MainActivity)getActivity()).getCurrentCommunicationDevice();
-            if (communicationDevice != null) {
-                communicationDevice.endConnection();
-            }
-        });
-
-        FloatingActionButton switchCommunicationButton = view.findViewById(R.id.switchCommunication);
-        switchCommunicationButton.setOnClickListener(v -> {
-            ((MainActivity)getActivity()).switchCurrentCommunicationDevice();
-            updateCommunicationUI(switchCommunicationButton);
-        });
 
         upCommand = new MoveByCommand(1,0);
         view.findViewById(R.id.upButton).setOnClickListener(v -> ((MainActivity)getActivity()).sendCommand(upCommand));
@@ -136,10 +74,81 @@ public class ConnectionViewFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Update fragment
+
+        initRobotSelectionDropdown();
+        initLoggerTextView();
+        // Init filter dropdown
+        filterChanged();
+
+        initButtonCallbacks();
+
         FloatingActionButton switchCommunicationButton = view.findViewById(R.id.switchCommunication);
+        // Update fragment
         updateCommunicationUI(switchCommunicationButton);
         setInfoVisible(false); // Disable UI at start
+    }
+
+    private void initButtonCallbacks() {
+        getView().findViewById(R.id.hide_ui).setOnClickListener(v -> {
+            setInfoVisible(!isInfoVisible);
+        });
+
+        getView().findViewById(R.id.clean_text).setOnClickListener(v -> {
+            protoMsgViewModel.clearLogs();
+            dataReceived.setText("");
+        });
+
+        getView().findViewById(R.id.connectButton).setOnClickListener(v -> {
+            CommunicationDevice communicationDevice = ((MainActivity)getActivity()).getCurrentCommunicationDevice();
+            if (communicationDevice != null) {
+                communicationDevice.establishConnection();
+            }
+        });
+        getView().findViewById(R.id.disconnectButton).setOnClickListener(v -> {
+            CommunicationDevice communicationDevice = ((MainActivity)getActivity()).getCurrentCommunicationDevice();
+            if (communicationDevice != null) {
+                communicationDevice.endConnection();
+            }
+        });
+
+        FloatingActionButton switchCommunicationButton = getView().findViewById(R.id.switchCommunication);
+        switchCommunicationButton.setOnClickListener(v -> {
+            ((MainActivity)getActivity()).switchCurrentCommunicationDevice();
+            updateCommunicationUI(switchCommunicationButton);
+        });
+    }
+
+    private void initRobotSelectionDropdown() {
+        Spinner dropdown = getView().findViewById(R.id.loggingFilter);
+        protoMsgViewModel.getProtoMsgStorerList().observe(getViewLifecycleOwner(), storers -> {
+            ArrayList<String> storerNames = new ArrayList<>();
+            storers.forEach(protoMsgStorer -> storerNames.add(protoMsgStorer.getUniqueName()));
+            ArrayAdapter<String> adapter =  new ArrayAdapter<>(requireActivity().getBaseContext(), android.R.layout.simple_spinner_dropdown_item, storerNames.toArray(new String[0]));
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dropdown.setAdapter(adapter);
+            if (protoMsgViewModel.getCurrentProtoMsgStorer().getValue() != null) {
+                String selectedDevice = protoMsgViewModel.getCurrentProtoMsgStorer().getValue().getUniqueName();
+                if (selectedDevice != null) {
+                    dropdown.setSelection(adapter.getPosition(selectedDevice));
+                }
+            }
+        });
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View currentView, int i, long l) {
+                protoMsgViewModel.setCurrentProtoMsgStorer( protoMsgViewModel.getProtoMsgStorerList().getValue().get(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+    }
+
+    private void initLoggerTextView() {
+        dataReceived.setMovementMethod(new ScrollingMovementMethod());
+        dataReceived.setText(protoMsgViewModel.getLastMsgsSpannable(MSG_LOGGING_LENGTH));
+        // Align text correctly on change logging filter
+        protoMsgViewModel.getCurrentProtoMsgStorer().observe(getViewLifecycleOwner(), this::onChanged);
     }
 
     private void filterChanged() {
@@ -163,7 +172,7 @@ public class ConnectionViewFragment extends Fragment {
             currentFragment = new TcpSettingsFragment(((TCPDeviceServer)communicationDevice).getServerAddress(), ((TCPDeviceServer)communicationDevice).getServerPort());
             button.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.wifi_icon));
         }
-        FragmentTransaction ft = fragmentManager.beginTransaction();
+        FragmentTransaction ft = getChildFragmentManager().beginTransaction();
         ft.replace(R.id.communicationContainer, currentFragment);
         ft.commit();
         // Invalidate view to update UI
@@ -185,5 +194,9 @@ public class ConnectionViewFragment extends Fragment {
         FrameLayout fl = v.findViewById(R.id.communicationContainer);
         fl.setVisibility(isVisible ? View.VISIBLE : View.GONE);
         v.invalidate();
+    }
+
+    private void onChanged(ProtoMsgStorer s) {
+        filterChanged();
     }
 }
