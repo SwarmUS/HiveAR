415f2fe56643a1e6a1a5479dee73d000228de35d
Thierry Constantin <thierry.constantin@usherbrooke.ca>
2021-09-15 18:19:03 -0400

    Saved settings thourhg viewmodel

--- HiveAR/app/src/main/java/com/swarmus/hivear/fragments/BoardNetworkConfig.java
+++ HiveAR/app/src/main/java/com/swarmus/hivear/fragments/BoardNetworkConfig.java
@@ -3,7 +3,10 @@ package com.swarmus.hivear.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +15,17 @@ import android.widget.RadioGroup;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.swarmus.hivear.R;
+import com.swarmus.hivear.viewmodels.AgentListViewModel;
+import com.swarmus.hivear.viewmodels.BoardNetworkConfigViewModel;
+
 public class BoardNetworkConfig extends Fragment {
     public static final String TAB_TITLE = "Board Network";
+    BoardNetworkConfigViewModel boardNetworkConfigViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        boardNetworkConfigViewModel = new ViewModelProvider(requireActivity()).get(BoardNetworkConfigViewModel.class);
     }
 
     @Override
@@ -30,6 +38,44 @@ public class BoardNetworkConfig extends Fragment {
         TextInputEditText ssidInput = v.findViewById(R.id.networkSSIDInputField);
         TextInputEditText pswdInput = v.findViewById(R.id.networkPSWDInputField);
         RadioGroup networkRole = v.findViewById(R.id.networkRole);
+
+        if (ssidInput != null)
+        {
+            ssidInput.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    String ssidInputValue = ssidInput.getText().toString();
+                    boardNetworkConfigViewModel.setNetworkdSSID(ssidInputValue);
+                }
+            });
+
+            ssidInput.setText(boardNetworkConfigViewModel.getNetworkSSID().getValue());
+        }
+
+        if (pswdInput != null)
+        {
+            pswdInput.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    String pswdInputValue = pswdInput.getText().toString();
+                    boardNetworkConfigViewModel.setNetworkdPSWD(pswdInputValue);
+                }
+            });
+
+            pswdInput.setText(boardNetworkConfigViewModel.getNetworkSSID().getValue());
+        }
 
         configureBoardButton.setOnClickListener(view -> {
             // Send config to board, should be by usb, but look into that...
