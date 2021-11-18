c06c820394cffe1950e05142329ba38946373b28
Thierry Constantin <thierry.constantin@usherbrooke.ca>
2021-11-18 09:59:45 -0500

    FUNCTIONNING AR COMMANDS

--- HiveAR/app/src/main/java/com/swarmus/hivear/adapters/ARCommandsAdapter.java
+++ HiveAR/app/src/main/java/com/swarmus/hivear/adapters/ARCommandsAdapter.java
@@ -6,22 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.swarmus.hivear.R;
 import com.swarmus.hivear.activities.MainActivity;
-import com.swarmus.hivear.databinding.ArCommandArgumentBinding;
 import com.swarmus.hivear.models.FunctionTemplate;
 import com.swarmus.hivear.models.FunctionTemplateArgument;
 import com.swarmus.hivear.models.FunctionTemplateList;
@@ -76,8 +73,32 @@ public class ARCommandsAdapter extends RecyclerView.Adapter<ARCommandsAdapter.AR
                     DataBindingUtil.inflate(inflater, R.layout.ar_command_argument, holder.cardView, false);
             argViewBinding.setFunctionArg(arg);
 
-            EditText argInput = argViewBinding.getRoot().findViewById(R.id.command_argument_value);
-            argInput.requestFocus();
+            TextView argInput = argViewBinding.getRoot().findViewById(R.id.command_argument_value);
+            FloatingActionButton decrease = argViewBinding.decreaseValue;
+            decrease.setOnClickListener(v -> {
+                if (arg.getArgumentType().equals(Integer.class)) {
+                    int value = Integer.parseInt(arg.getValue());
+                    argInput.setText(String.valueOf(value - 1));
+                }
+
+                if (arg.getArgumentType().equals(Float.class)) {
+                    float value = Float.parseFloat(arg.getValue());
+                    argInput.setText(String.valueOf(value - 0.5));
+                }
+            });
+
+            FloatingActionButton increase = argViewBinding.increaseValue;
+            increase.setOnClickListener(v -> {
+                if (arg.getArgumentType().equals(Integer.class)) {
+                    int value = Integer.parseInt(arg.getValue());
+                    argInput.setText(String.valueOf(value + 1));
+                }
+
+                if (arg.getArgumentType().equals(Float.class)) {
+                    float value = Float.parseFloat(arg.getValue());
+                    argInput.setText(String.valueOf(value + 0.5));
+                }
+            });
 
             if (arg.getArgumentType().equals(Integer.class)) {
                 argInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
